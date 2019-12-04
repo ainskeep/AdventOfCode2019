3fbaaeadbcb21c28a633a7e16de0a16b92dcb71c
Alex Inskeep <inskeep.alex@gmail.com>
2019-12-03 20:41:47 -0700

    Final solution for day 3

--- 03/DayThree.py
+++ 03/DayThree.py
@@ -1,6 +1,5 @@
 FILE_PATH = "input.txt"
 
-
 def buildCordSet(lineString):
     lineSet = {(0, 0)}
     curLoc = [0,0]
@@ -52,46 +51,43 @@ for cord in crossings:
 
 print(f"PART One: Minimum Cord: {minCord}. Manhattan Distance: {abs(minCord[0]) + abs(minCord[1])}")
 
-# Above, I'm preserving the way I solved part 1, but I need to do it a bit differently in order to solve part 2
-# Sets are not the best way to solve part 2
-
 # part 2
 
+# I needed to rewrite the buildSet into a buildDict
 
-def buildCordSet2(lineString):
-    lineSet = {(0, 0, 0)}
+def buildCordDict(lineString):
+    lineDict = {(0, 0): 0}
     curLoc = [0,0]
     totalSteps = 0
     for dirSet in lineString.split(','):
         if dirSet.startswith('R'):
             steps = int(dirSet.replace('R', ''))
             for x in range(1, steps+1):
-                lineSet.add((curLoc[0]+x, curLoc[1], totalSteps+x))
+                lineDict[(curLoc[0]+x, curLoc[1])] = totalSteps + x
             curLoc[0] += steps
             totalSteps += steps
         elif dirSet.startswith('D'):
             steps = int(dirSet.replace('D', ''))
             for y in range(1, steps+1):
-                lineSet.add((curLoc[0], curLoc[1]-y, totalSteps+y))
-            curLoc[1] = curLoc[1] - steps
-            curLoc[0] += steps
+                lineDict[(curLoc[0], curLoc[1]-y)] = totalSteps+y
+            curLoc[1] -= steps
             totalSteps += steps
         elif dirSet.startswith('L'):
             steps = int(dirSet.replace('L', ''))
             for x in range(1, steps+1):
-                lineSet.add((curLoc[0]-x, curLoc[1], totalSteps+x))
+                lineDict[(curLoc[0]-x, curLoc[1])] = totalSteps + x
             curLoc[0] -= steps
             totalSteps += steps
         elif dirSet.startswith('U'):
             steps = int(dirSet.replace('U', ''))
             for y in range(1, steps+1):
-                lineSet.add((curLoc[0], curLoc[1]+y, totalSteps+y))
+                lineDict[(curLoc[0], curLoc[1] + y)] = totalSteps + y
             curLoc[1] += steps
             totalSteps += steps
         else:
             print(f"unrecognized input: {dirSet}")
 
-    return lineSet
+    return lineDict
 
 lineList = []
 
@@ -99,15 +95,18 @@ with open(FILE_PATH) as inFile:
     for line in inFile:
         lineList.append(line.strip())
 
-LineOneSet = buildCordSet2(lineList[0])
-LineTwoSet = buildCordSet2(lineList[1])
+LineOneDict = buildCordDict(lineList[0])
+LineTwoDict = buildCordDict(lineList[1])
 
-minCord = (0, 0, 999999999999)
+keyIntersection = set(LineOneDict.keys()) & set(LineTwoDict.keys())
 
-for cordOne in LineOneSet:
-    for cordTwo in LineTwoSet:
-        if cordOne[0] == cordTwo[0] and cordOne[1] == cordTwo[1]:
-            if cordOne[2] + cordTwo[2] < minCord[2]:
-                minCord = cordOne
+minDistance = -1
 
-print(f"PART 2: Minimum Cord: {minCord}. Line Distance: {minCord[2]}")
+for key in keyIntersection:
+    if minDistance == -1 and key != (0, 0):
+        minDistance = LineOneDict[key] + LineTwoDict[key]
+
+    if LineOneDict[key] + LineTwoDict[key] < minDistance and key != (0, 0):
+        minDistance = LineOneDict[key] + LineTwoDict[key]
+
+print(f"Min Line distance: {minDistance}")
--- 03/README.md
+++ 03/README.md
@@ -1,1 +1,4 @@
 # [Day 3](https://adventofcode.com/2019/day/3)
+
+Day 3 was a valuable lesson in the power of hashable data constructs when it comes to searching. I tried doing this in a 
+list out of lazyness but that program took way too long
