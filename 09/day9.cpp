6b18634ed77bb1dd9404cec095880fb3912ecf48
AlexInskeep <ainskeep@cradlepoint.com>
2019-12-09 18:41:34 -0700

    day9 work transfer

--- 09/day9.cpp
+++ 09/day9.cpp
@@ -9,16 +9,35 @@
 #include <sstream>
 using namespace std;
 
-const string filePath = "./testInput.txt";
+const string filePath = "/home/ainskeep/github/AdventOfCode2019/09/input.txt";
 
-long long runProgram (){
+long long int getParam(long long int *array, int paramMode, int paramNumber, int opPosition, int relativeBase){
+    int param;
+        switch(paramMode){
+            case 0:
+                // position mode
+                param = array[opPosition+paramNumber];
+                break;
+            case 1:
+                // imediate mode
+                param = opPosition+paramNumber;
+                break;
+            case 2:
+                // relative mode
+                param = relativeBase + array[opPosition+paramNumber];
+                break;
+        };
+    return param;
+}
+
+long long int runProgram (){
     string line, code;
     ifstream inFile (filePath);
     getline(inFile, line); //get the first line
 
     stringstream ss(line);
 
-    long long arraySize = 0;
+    long long int arraySize = 0;
     while ( getline(ss, code, ',') ){ //find how many numbers there are in the array
         arraySize++;
     }
@@ -26,29 +45,30 @@ long long runProgram (){
     ss.str(line); //reset the string stream
     ss.clear();
 
-    long long array[arraySize*10]; //create an array of just the right size
+    long long int array[arraySize * 10] = {0}; //create an array of just the right size
 
+    char* pEnd;
     int i = 0;
     while ( getline(ss, code, ',') ){
-        array[i] = strtoll(code);
+        array[i] = strtoll(code.c_str(), &pEnd,10);
         i++;
     }
 
     //main program
     int opCode = 0;
     unsigned int instruction = 0;
-    long long opPosition = 0;
+    long long int opPosition = 0;
 
     int paramMode1 = -1;
     int paramMode2 = -1;
     int paramMode3 = -1;
 
-    long long param1 = 0;
-    long long param2 = 0;
-    long long param3 = 0;
+    long long int param1 = 0;
+    long long int param2 = 0;
+    long long int param3 = 0;
 
-    long long inputInt = 0;
-    long long relativeBase = 0;
+    long long int inputInt = 0;
+    long long int relativeBase = 0;
 
     bool breakLoop = false;
     while( !breakLoop ){
@@ -60,60 +80,20 @@ long long runProgram (){
         paramMode2 = instruction/1000 % 10;
         paramMode3 = instruction/10000 % 10;
 
-        switch(paramMode1){
-            case 0:
-                // position mode
-                param1 = array[array[opPosition+1]];
-                break;
-            case 1:
-                // imediate mode
-                param1 = array[opPosition+1];
-                break;
-            case 2:
-                // relative mode
-                param1 = array[ relativeBase + array[opPosition+1]];
-                break;
-        };
-        switch(paramMode2){
-            case 0:
-                // position mode
-                param2 = array[array[opPosition+2]];
-                break;
-            case 1:
-                // imediate mode
-                param2 = array[opPosition+2];
-                break;
-            case 2:
-                // relative mode
-                param2 = array[ relativeBase + array[opPosition+2]];
-                break;
-        };
-        switch(paramMode3){
-            case 0:
-                // position mode
-                param1 = array[array[opPosition+3]];
-                break;
-            case 1:
-                // imediate mode
-                param1 = array[opPosition+3];
-                break;
-            case 2:
-                // relative mode
-                param1 = array[ relativeBase + array[opPosition+3]];
-                break;
-        };
-
-
         switch(opCode){
             case 1:
-                param3 = array[opPosition+3]; // never immediate mode
+                param1 = array[getParam(array, paramMode1, 1, opPosition, relativeBase)];
+                param2 = array[getParam(array, paramMode2, 2, opPosition, relativeBase)];
+                param3 = getParam(array, paramMode3, 3, opPosition, relativeBase); //unsure
 
                 array[param3] = param1 + param2;
                 opPosition += 4;
 
                 break;
             case 2:
-                param3 = array[opPosition+3]; // never immediate mode
+                param1 = array[getParam(array, paramMode1, 1, opPosition, relativeBase)];
+                param2 = array[getParam(array, paramMode2, 2, opPosition, relativeBase)];
+                param3 = getParam(array, paramMode3, 3, opPosition, relativeBase); //unsure
 
                 array[param3] = param1 * param2;
                 opPosition += 4;
@@ -123,7 +103,7 @@ long long runProgram (){
                 cin >> inputInt;
                 cout << "You entered: " << inputInt << endl;
 
-                param1 = array[opPosition+1];
+                param1 = array[getParam(array, paramMode1, 1, opPosition, relativeBase)];
 
                 array[param1] = inputInt;
 
@@ -131,11 +111,16 @@ long long runProgram (){
                 break;
 
             case 4:
+                param1 = array[getParam(array, paramMode1, 1, opPosition, relativeBase)];
+
                 cout << param1 << endl;
                 opPosition += 2;
                 break;
 
             case 5:
+                param1 = array[getParam(array, paramMode1, 1, opPosition, relativeBase)];
+                param2 = array[getParam(array, paramMode2, 2, opPosition, relativeBase)];
+
                 if (param1 != 0){
                     opPosition = param2;
                 }else {
@@ -143,6 +128,8 @@ long long runProgram (){
                 }
                 break;
             case 6:
+                param1 = array[getParam(array, paramMode1, 1, opPosition, relativeBase)];
+                param2 = array[getParam(array, paramMode2, 2, opPosition, relativeBase)];
 
                 if (param1 == 0){
                     opPosition = param2;
@@ -151,7 +138,10 @@ long long runProgram (){
                 }
                 break;
             case 7:
-                param3 = array[opPosition+3]; // never immediate mode
+                param1 = array[getParam(array, paramMode1, 1, opPosition, relativeBase)];
+                param2 = array[getParam(array, paramMode2, 2, opPosition, relativeBase)];
+                param3 = getParam(array, paramMode3, 3, opPosition, relativeBase); //unsure
+
 
                 if (param1 < param2) {
                     array[param3] = 1;
@@ -162,7 +152,9 @@ long long runProgram (){
                 opPosition += 4;
                 break;
             case 8:
-                param3 = array[opPosition+3]; // never immediate mode
+                param1 = array[getParam(array, paramMode1, 1, opPosition, relativeBase)];
+                param2 = array[getParam(array, paramMode2, 2, opPosition, relativeBase)];
+                param3 = getParam(array, paramMode3, 3, opPosition, relativeBase); //unsure
 
                 if (param1 == param2) {
                     array[param3] = 1;
@@ -173,8 +165,7 @@ long long runProgram (){
                 opPosition += 4;
                 break;
             case 9:
-                param1 = paramMode1 == 1 ? array[opPosition+1] :  array[array[opPosition+1]];
-
+                param1 = getParam(array, paramMode1, 1, opPosition, relativeBase);
                 relativeBase += param1;
 
                 opPosition += 2;
@@ -196,7 +187,7 @@ long long runProgram (){
 
 int main()
 {
-    long long ans = runProgram();
+    long long int ans = runProgram();
 
     return 0;
 }
